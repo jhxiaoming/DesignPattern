09373816eef5fb44175115b700afe4709fce7ff6
Peter <409132121@qq.com>
2018-07-19 17:59:02 +0800

    Initial commit

---
+++ strategy/src/Duck.java
@@ -0,0 +1,11 @@
+public abstract class Duck {
+    public void quack() {
+        System.out.println("i can quack");
+    }
+
+    public void swim() {
+        System.out.println("i can swim");
+    }
+
+    public abstract void display();
+}
